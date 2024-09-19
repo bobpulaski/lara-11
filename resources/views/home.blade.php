e7fcc27472d2c599c78452920b61725e850ddff5
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2024-09-19 14:42:58 +0300

    добавил на главную блок с двумя колонкми

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout title="Home Title!">
-    <h1 class="text-3xl font-bold underline">Home page</h1>
+    <div class="flex flex-col md:flex-row bg-gray-100">
+        <div class="h-32 w-32 mt-16 mx-auto">1</div>
+        <div class="h-32 w-32 mt-16 mx-auto">2</div>
+    </div>
 </x-layout>
